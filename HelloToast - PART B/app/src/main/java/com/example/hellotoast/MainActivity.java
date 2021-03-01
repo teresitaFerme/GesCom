02aad98886ed9a740629a7455776c875c2b744c6
teresitaFerme <60939567+teresitaFerme@users.noreply.github.com>
2021-03-01 18:48:02 +0100

    Count cambia color
    Segun si el counter es par o impar el boton de count cambia su color

--- HelloToast - PART B/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast - PART B/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,19 +2,23 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    private Button b_count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        b_count = (Button) findViewById(R.id.button_count);
     }
 
     public void show_toast(View view) {
@@ -25,8 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void show_count(View view) {
         mCount++;
-        if (mShowCount != null)
+
+        if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
+            if (mCount % 2 == 0)   b_count.setBackgroundColor(Color.BLACK);
+            else b_count.setBackgroundColor(Color.BLUE);
+        }
     }
 
     public void reset(View view) {
