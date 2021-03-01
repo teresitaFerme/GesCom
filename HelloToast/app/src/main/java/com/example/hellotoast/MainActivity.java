eb58bc0afbd2e694f404a779305f6f3e08b092d3
teresitaFerme <60939567+teresitaFerme@users.noreply.github.com>
2021-03-01 18:23:35 +0100

    Terminado
    1.2 PART A

--- HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int mCount = 0;
+    private TextView mShowCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void show_toast(View view) {
+        Toast toast = Toast.makeText(this, "Hello Toast",
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void show_count(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
