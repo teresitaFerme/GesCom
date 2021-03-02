5e0ffae7ef8229e0486ce7e886754f58946e44d7
teresitaFerme <60939567+teresitaFerme@users.noreply.github.com>
2021-03-02 18:18:57 +0100

    Send + Reply

--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +20,14 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+        mReply = findViewById(R.id.editText_second);
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
