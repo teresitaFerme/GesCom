3c198f0f8b61e9445c48008b8e685ef4c3203cd1
teresitaFerme <60939567+teresitaFerme@users.noreply.github.com>
2021-03-13 17:55:22 +0100

    Login Usuario Layout
    El layout funciona, solo falta la conexión a la bbdd

--- GesCom/app/src/main/java/es/ucm/fdi/gescom/MainActivity.java
+++ GesCom/app/src/main/java/es/ucm/fdi/gescom/MainActivity.java
@@ -2,7 +2,9 @@ package es.ucm.fdi.gescom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        launchLogin();
+    }
+
+    private void launchLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        //aquí habria que devolver lo que devuelva el login
     }
 }
