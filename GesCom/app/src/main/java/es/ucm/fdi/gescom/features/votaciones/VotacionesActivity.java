9a77421b863202e8063e72fe4554cf5cfd239071
teresitaFerme <60939567+teresitaFerme@users.noreply.github.com>
2021-04-27 20:07:54 +0200

    Siguiente intento de votaciones

--- GesCom/app/src/main/java/es/ucm/fdi/gescom/features/votaciones/VotacionesActivity.java
+++ GesCom/app/src/main/java/es/ucm/fdi/gescom/features/votaciones/VotacionesActivity.java
@@ -1,8 +1,11 @@
 package es.ucm.fdi.gescom.features.votaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -12,6 +15,9 @@ import es.ucm.fdi.gescom.base.BaseActivity;
 public class VotacionesActivity extends BaseActivity implements VotacionesView{
     private VotacionesPresenter mPresenter;
     private FloatingActionButton mFab;
+    private RecyclerView mVotacionesPendientes, mVotacionesAnteriores;
+    private Button mEnviarVoto;
+    private ImageButton mFavor, mContra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,14 @@ public class VotacionesActivity extends BaseActivity implements VotacionesView{
         setContentView(R.layout.activity_votaciones);
 
         mPresenter = new VotacionesPresenter(this);
+
         mFab = findViewById(R.id.votaciones_admin_fab);
+
+        //TODO ESTO DEBERÍA IR EN EL VIEWHOLDER
+        mVotacionesPendientes = findViewById(R.id.recycler_votaciones_pendientes);
+        mVotacionesAnteriores = findViewById(R.id.recycler_votaciones_anteriores);
+        mEnviarVoto = findViewById(R.id.votacion_button);
+        mFavor = findViewById(R.id.votacion_button_favor);
+        mContra = findViewById(R.id.votacion_button_contra);
     }
 }
