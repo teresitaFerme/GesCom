40f36cfc71e22da9e9e1f65ca47e182df2ef45bb
teresitaFerme <60939567+teresitaFerme@users.noreply.github.com>
2021-05-03 22:53:02 +0200

    Merged register user functionality for simplicity

--- GesCom/app/src/main/java/es/ucm/fdi/gescom/features/register_user/RegisterUserModel.java
+++ GesCom/app/src/main/java/es/ucm/fdi/gescom/features/register_user/RegisterUserModel.java
@@ -1,5 +1,6 @@
 package es.ucm.fdi.gescom.features.register_user;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -7,6 +8,7 @@ import android.provider.BaseColumns;
 
 import es.ucm.fdi.gescom.base.BaseModel;
 import es.ucm.fdi.gescom.datacache.GesComApp;
+import es.ucm.fdi.gescom.datacache.User;
 import es.ucm.fdi.gescom.sqlite.CommunitiesDatabase;
 import es.ucm.fdi.gescom.sqlite.CommunitiesDatabaseHelper;
 
@@ -46,5 +48,54 @@ public class RegisterUserModel extends BaseModel {
             }
             else return false;
         } else return false;
+    }
+
+    public boolean getUsername(String username) {
+        SQLiteDatabase db = mCommunitiesDBHelper.getReadableDatabase();
+
+        String[] projection = {
+                BaseColumns._ID,
+                CommunitiesDatabase.User.COLUMN_NAME_USERNAME
+        };
+
+        String selection = CommunitiesDatabase.User.COLUMN_NAME_USERNAME + " = ?";
+        String[] selectionArgs = {username};
+
+        Cursor cursor = db.query(
+                CommunitiesDatabase.User.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null
+        );
+
+        return cursor.moveToFirst();
+    }
+
+    public void registerUser(String username, String pss, String dni) {
+        SQLiteDatabase db = mCommunitiesDBHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(CommunitiesDatabase.User.COLUMN_NAME_USERNAME, username);
+        values.put(CommunitiesDatabase.User.COLUMN_NAME_PASSWORD, pss);
+
+        String selection = CommunitiesDatabase.User.COLUMN_NAME_USERNAME + " = ?";
+        String[] selectionArgs = {dni};
+
+        db.update(
+                CommunitiesDatabase.User.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs
+        );
+
+        String[] projection = {
+                BaseColumns._ID,
+                CommunitiesDatabase.User.COLUMN_NAME_LOCALIZER
+        };
+        selectionArgs = new String[]{username};
+        GesComApp.setUser(new User(username, pss, db.query(CommunitiesDatabase.User.TABLE_NAME, projection, selection, selectionArgs, null, null, null).getString(1), db.query(CommunitiesDatabase.User.TABLE_NAME, projection, selection, selectionArgs, null, null, null).getInt(0)));
     }
 }
--- GesCom/app/src/main/java/es/ucm/fdi/gescom/features/register_user/RegisterUserPresenter.java
+++ GesCom/app/src/main/java/es/ucm/fdi/gescom/features/register_user/RegisterUserPresenter.java
@@ -15,10 +15,23 @@ public class RegisterUserPresenter extends BasePresenter {
 
     public void validate(String dni, String clave) {
         if(dni.length() != 0 && clave.length() != 0){
-            if(mModel.validateData(dni, clave)) mView.correctData();
-            else mView.wrongData("Dni o clave incorrectos");
+            if(!mModel.validateData(dni, clave)) mView.wrongData("Dni o clave incorrectos");
         }else{
             mView.wrongData("Por favor rellene todos los campos");
         }
+    }
+
+    public void validate(String username, String pass, String passRepeat, String dni) {
+        //TODO meter validaciones de username (sin signos) y de las contraseñas
+        if (username.length() != 0 && pass.length() != 0 && passRepeat.length() != 0) {
+            if(pass.equals(passRepeat)){
+                if(pass.length() >= 8){
+                    if (!mModel.getUsername(username)) {
+                        mModel.registerUser(username, pass, dni);
+                        mView.initSuccessful();
+                    } else mView.initFailure("Ese usuario ya está cogido. Introduzca otro usuario.");//TODO borrar aqui el campo usuario
+                }else  mView.initFailure("La contraseña debe tener 8 carácteres como mínimo.");
+            }else mView.initFailure("Las contraseñas no coinciden");//TODO hacer que se borren los campos de las contraseñas
+        }else mView.initFailure("Por favor rellene todos los campos");
     }
 }
